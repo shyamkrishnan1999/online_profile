6b07f16a7180c32bb9b942c36700c1d9f763b712
CLAB1 <rittyt@yahoo.com>
2019-07-11 16:05:58 +0530

    yo

--- portal/online_portal/models.py
+++ portal/online_portal/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True,editable=False)
+    updated_at=models.DateTimeField(auto_now_add=True,editable=False)
+
+class account(TimeStampedModel):
+    name=models.CharField(max_length=20)
+    email=models.CharField(max_length=50)
+    dob=models.DateTimeField()
+    blood=models.CharField(max_length=20)
+    password=models.CharField(max_length=20)
+
+
+
